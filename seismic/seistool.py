3e7cd25b87948e76c81198137469ea80f99568f1
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2025-08-10 21:31:40 +0200

    create parse_header

--- seismic/seistool.py
+++ seismic/seistool.py
@@ -1163,3 +1163,57 @@ def slicePostStackInversion(data, type, wav, m0,
     # m_relative = np.nan
 
   return m_relative, r_relative
+
+def parseHeader(filename):
+  """
+  Parse header of a SEGY seismic volume
+  """
+  import segyio
+  import re
+  import pandas as pd
+
+  def parse_trace_headers(segyfile, n_traces):
+      '''
+      Parse the segy file trace headers into a pandas dataframe.
+      Column names are defined from segyio internal tracefield
+      One row per trace
+      '''
+      # Get all header keys
+      headers = segyio.tracefield.keys
+      # Initialize dataframe with trace id as index and headers as columns
+      df = pd.DataFrame(index=range(1, n_traces + 1),
+                        columns=headers.keys())
+      # Fill dataframe with all header values
+      for k, v in headers.items():
+          df[k] = segyfile.attributes(v)[:]
+      return df
+
+
+  def parse_text_header(segyfile):
+      '''
+      Format segy text header into a readable, clean dict
+      '''
+      raw_header = segyio.tools.wrap(segyfile.text[0])
+      # Cut on C*int pattern
+      cut_header = re.split(r'C ', raw_header)[1::]
+      # Remove end of line return
+      text_header = [x.replace('\n', ' ') for x in cut_header]
+      text_header[-1] = text_header[-1][:-2]
+      # Format in dict
+      clean_header = {}
+      i = 1
+      for item in text_header:
+          key = "C" + str(i).rjust(2, '0')
+          i += 1
+          clean_header[key] = item
+      return clean_header  
+
+  
+  with segyio.open(filename, iline=189, xline=193) as f:  
+    # Load headers
+    n_traces = f.tracecount    
+    bin_headers = f.bin
+    text_headers = parse_text_header(f)
+    trace_headers = parse_trace_headers(f, n_traces)
+
+  return trace_headers 
